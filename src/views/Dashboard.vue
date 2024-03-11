0ccaa26a0510b80f933cd72984b43212eb2b8c53
Gourab Mahapatra <mgourab@hotmail.com>
2024-03-11 09:06:48 +0530

    - fixed chart hours for today

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -180,16 +180,10 @@ const getStartDate = () => {
       break;
     case 'today':
     default:
-      
       today.setHours(0, 0, 0, 0); // Set time to 00:00:00
-      const yesterday = new Date(today);
-      yesterday.setDate(yesterday.getDate() - 1); // Set to yesterday
-      timeInterval = yesterday.getTime(); // Start from yesterday 12 AM
+      timeInterval = today.getTime(); // Start from today 12 AM
       title = 'Orders Today';
-      xaxis = [
-        '12AM', '1AM', '2AM', '3AM', '4AM', '5AM', '6AM', '7AM', '8AM', '9AM', '10AM', '11AM',
-        '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6AM', '7PM', '8PM', '9PM', '10PM', '11PM',
-      ];
+      xaxis = Array.from({ length: 24 }, (_, i) => `${i % 12 === 0 ? 12 : i % 12}${i < 12 ? 'AM' : 'PM'}`);
   }
 
   return {timeInterval, title, xaxis};
